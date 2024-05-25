f0095599b47387b46ee07fe4630133916d4b15aa
Janhavi Singh <janhavis1111@gmail.com>
2024-05-25 08:17:52 +0530

    Added basicReact with Vite

---
+++ basicReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    <h2>First Code In React with Vite</h2>
+    </>
+  )
+}
+
+export default App
---
+++ basicReact/src/Chai.jsx
@@ -0,0 +1,8 @@
+
+function Chai(){
+  return (
+    <h1>Hey!</h1>
+  )
+}
+
+export default Chai;
